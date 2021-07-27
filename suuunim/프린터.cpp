0e2a216b293c5b77cba8c9245e229a02b087f0fe
suuunim <87135671+suuunim@users.noreply.github.com>
2021-07-27 17:18:24 +0900

    프린터.cpp

---
+++ suuunim/프린터.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+     int answer = 0;
+     priority_queue<int> q2;
+     queue<pair<int,int>> q;
+    
+   
+    
+    for(int i=0;i<priorities.size();i++){
+        q.push(make_pair(i,priorities[i]));
+        q2.push(priorities[i]);
+    }
+    
+    while(!q.empty()){
+        if(q.front().second==q2.top())
+            if(q.front().first==location)
+                return answer+1;
+            else{
+                answer++;
+                q.pop();
+                q2.pop();
+            }
+        else{
+            q.push(q.front());
+            q.pop();
+        }
+        
+        
+    }
+    
+    
+    
+    return answer;
+}
