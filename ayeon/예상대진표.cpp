da17bd353f87c78cfe96b1d186cde14d101ae06f
woozoo_zinc <77188666+yourzinc@users.noreply.github.com>
2021-08-03 20:28:13 +0900

    week 4

--- ayeon/예상대진표.cpp
+++ ayeon/예상대진표.cpp
@@ -1,3 +1,4 @@
+/* FIN */
 #include <iostream>
 #include <cmath>
 
@@ -11,23 +12,23 @@ int solution(int n, int a, int b)
     {   t=a; a=b; b=t;  }
 
     while(a!=b){
-    while(a<b && b<=n/2)
-    {
-        n/=2;
-    }       
-    while(n/2<a && a<b)
-    {
-        a = a-n/2;
-        b = b-n/2;
-        n /= 2;
-    }
-    while(a<=n/2 && n/2 <b)
-    {
-        answer++; //1
-        a = (a-1)/2+1;
-        b = (b-1)/2+1;
-        n/=2;
-    }
+        while(a<b && b<=n/2)
+        {
+            n/=2;
+        }       
+        while(n/2<a && a<b)
+        {
+            a = a-n/2;
+            b = b-n/2;
+            n /= 2;
+        }
+        while(a<=n/2 && n/2 <b)
+        {
+            answer++; //1
+            a = (a-1)/2+1;
+            b = (b-1)/2+1;
+            n/=2;
+        }
     }
     return answer;
 }
